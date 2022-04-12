26412495febc6560e15b2122c04c546b38c3a1c8
jdpaquette <josephdpaquette@gmail.com>
2022-04-12 14:11:31 -0400

    Day One Band Name Generator

---
+++ Day_One/band.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator')
+city = input('What city were you born in? ')
+pet = input('What is the name of your pet? ')
+print('Your suggested band name is ' + city + " " + pet + "!")
