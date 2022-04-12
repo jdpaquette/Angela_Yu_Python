21bae028c77d87692916a9fe131c8d85e6276a23
jdpaquette <josephdpaquette@gmail.com>
2022-04-12 14:31:43 -0400

    added new line break

--- Day_One/band.py
+++ Day_One/band.py
@@ -1,4 +1,7 @@
 print('Welcome to the Band Name Generator')
-city = input('What city were you born in? ')
-pet = input('What is the name of your pet? ')
+city = input('What city were you born in?\n')
+pet = input('What is the name of your pet?\n')
 print('Your suggested band name is ' + city + " " + pet + "!")
+
+# Angela's code:
+# print(len(input('What is your name?)))
