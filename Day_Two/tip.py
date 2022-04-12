834ca9a2ccb90049c9ec746817c245e57857a96a
jdpaquette <josephdpaquette@gmail.com>
2022-04-12 14:56:54 -0400

    Day Two

---
+++ Day_Two/tip.py
@@ -0,0 +1,9 @@
+print('Welcome to the Tip Calculator')
+total = float(input('What is the total bill amount? $'))
+guests = int(input('How many people to split the bill? '))
+tip = int(input('What percentage tip to give: 10, 15, 18, 20? '))
+each = ((total + (total*(tip/100))) / guests)
+print('Each person should pay $' + str(round(each, 2)))
+
+# Angela's Code
+# 
