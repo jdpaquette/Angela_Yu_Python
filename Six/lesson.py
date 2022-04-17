4b3f0b81ad6402d2181fae8fed12ee9968a4f29c
jdpaquette <josephdpaquette@gmail.com>
2022-04-17 14:20:12 -0400

    update day 6 lesson

--- Six/lesson.py
+++ Six/lesson.py
@@ -8,7 +8,7 @@ def my_function(): # creating / defining function
 
 my_function() # call the function
 
-# Reeborg's World
+# Reeborg's World (reeborg.ca)
 def turn_right():
     turn_left()
     turn_left()
@@ -26,3 +26,37 @@ def jump():
 
 for step in range(6):
     jump()
+
+# or using a while loop instead of for loop
+hurdles = 6
+while hurdles > 0:
+    jump()
+    hurdles -= 1
+# if we used at_goal() which is set to true...
+while not at_goal():
+    jump()
+
+# Reeborg's World Hurdles 3
+while not at_goal():
+    if not wall_in_front():
+        move()
+    else:
+        jump()
+
+# Reeborg's World Hurdle 4
+def jump():
+    turn_left()
+    while not right_is_clear():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while not wall_in_front():
+        move()
+    turn_left()
+
+while not at_goal():
+    if not wall_in_front():
+        move()
+    else:
+        jump()
