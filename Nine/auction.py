fbe4b51bd010e29b4487d4c9b5b4c19fd00c6e7d
jdpaquette <josephdpaquette@gmail.com>
2022-04-19 22:36:01 -0400

    initial code for auction

--- Nine/auction.py
+++ Nine/auction.py
@@ -2,15 +2,23 @@ from art import logo
 print(logo)
 
 all_bids = {}
-
 users = True
-while users:
-    name = input('What is your name? \n')
-    bid = int(input('What is your bid amount? \n'))
-    new_user = {}
-    new_user.update({name: bid})
-    all_bids.update(new_user)
 
+def winner(bidding_record):
+    highest_bid = 0
+    winner = ''
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f'The winner is {winner} with a bid of ${highest_bid}')
+
+while users:
+    name = input('What is your name?: ')
+    bid = int(input('What is your bid amount?: $'))
+    all_bids[name] = bid
     more_users = input('Are the any more users? Type yes or no: \n').lower()
     if more_users == 'no':
         users = False
+        winner(all_bids)
